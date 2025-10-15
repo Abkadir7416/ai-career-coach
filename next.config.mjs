0d6f9185a639f11de8ae27c7cfddbc755e04c74e
Abdul Kadir (Technology - New Delhi) <abdul.kadir@partner.htmedialabs.com>
2025-10-15 07:07:06 +0530

    Add Landing Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // Add an array of allowed external image domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me', // <-- Add this hostname
+        port: '',
+        pathname: '/api/portraits/**', // Use a wildcard if images are in a subdirectory
+      },
+      // You can add more domains here if needed
+    ],
+    // OR, for Next.js versions < 13.4, use the 'domains' property:
+    // domains: ['randomuser.me'],
+  },
+};
 
 export default nextConfig;
